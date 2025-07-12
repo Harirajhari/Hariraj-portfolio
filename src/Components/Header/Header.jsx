1bd08c0cdbd8003074105e7fb6dd1c4dd5c147e8
Harirajhari <hariraj121203@gmail.com>
2025-07-12 12:24:08 +0530

    resume bug resolve

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -26,7 +26,7 @@ const Header = () => {
           <a href="#projects" className="hover:text-violet-400 transition">Projects</a>
           <a href="#contact" className="hover:text-violet-400 transition">Contact</a>
           <a
-            href="https://drive.google.com/file/d/186XaZSDnqMqAynxh7TZGs70x0ouBpZya/view?usp=sharing"
+            href="https://drive.google.com/file/d/1FnoIngh9r8Ugu1-8WOO_wt4sKhbGkWyA/view?usp=sharing"
             download
             className="px-3 py-1 rounded-full border border-violet-500 hover:bg-violet-600 hover:text-white transition text-violet-400"
           >
